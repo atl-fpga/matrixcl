110763136da2f17c38bc0c580271cb119a59dc1c
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-10-28 11:11:45 +0900

    Refactored

--- host.cpp
+++ host.cpp
@@ -15,33 +15,12 @@
 #define WIDTH_A (16)
 #define HEIGHT_A (16)
 
-void buildProgram(cl::Context *context, cl::Program* program) {
-  try {
-    program->build();
-  } catch (cl::Error error) {
-    if (error.err() == CL_BUILD_PROGRAM_FAILURE) {
-      std::vector<cl::Device> devices;
-      devices = context->getInfo<CL_CONTEXT_DEVICES>();
-      std::string built = program->getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]);
-      std::cerr << built << "\n";
-    }
-    throw error;
-  }
-}
-
 void runMatrixVectorMul() {
   auto mat = matrix::randmat<WIDTH_A, HEIGHT_A>();
   auto vec = matrix::randvec<WIDTH_A>();
 
   mat.print();
   vec.print();
-
-  switch (DEVICE) {
-  case CL_DEVICE_TYPE_DEFAULT: printf("DEVICE=DEFAULT\n"); break;
-  case CL_DEVICE_TYPE_CPU:     printf("DEVICE=CPU\n"); break;
-  case CL_DEVICE_TYPE_GPU:     printf("DEVICE=GPU\n"); break;
-  default:                     printf("DEVICE=%d\n", DEVICE); break;
-  }
 
   matrix::op::multiply(mat, vec);
 }
@@ -53,6 +32,10 @@ void runMatrixMatrixMul() {
   matA.print();
   matB.print();
 
+  matrix::op::multiply(matA, matB);
+}
+
+int main(int argc, char** argv) {
   switch (DEVICE) {
   case CL_DEVICE_TYPE_DEFAULT: printf("DEVICE=DEFAULT\n"); break;
   case CL_DEVICE_TYPE_CPU:     printf("DEVICE=CPU\n"); break;
@@ -60,10 +43,6 @@ void runMatrixMatrixMul() {
   default:                     printf("DEVICE=%d\n", DEVICE); break;
   }
 
-  matrix::op::multiply(matA, matB);
-}
-
-int main(int argc, char** argv) {
   //runMatrixVectorMul();
   runMatrixMatrixMul();
   return 0;
