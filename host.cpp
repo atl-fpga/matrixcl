36a62c84c77ee3c54fc002d654227c333379f80c
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2015-10-28 12:02:14 +0900

    Adding benchmark mode

--- host.cpp
+++ host.cpp
@@ -37,6 +37,22 @@ void runMatrixMatrixMul() {
   result.print();
 }
 
+inline double estimated_performance_of(const int Mdim, const int Ndim, const int Pdim, const double run_time, const int iters = 1) {
+  return iters * 2.0 * Mdim * Ndim * Pdim/(1000000.0f * run_time);
+}
+
+void benchmark(const unsigned int iters) {
+  util::Timer timer;
+
+  auto mat = matrix::randmat<1024, 1024>();
+  for (int i=0; i<iters; ++i) {
+    mat = matrix::op::multiply(mat, matrix::randmat<1024, 1024>());
+  }
+
+  const double run_time = static_cast<double>(timer.getTimeMilliseconds()) / 1000.0;
+  printf(" %.2f seconds at %.1f MFLOPS \n",  run_time, estimated_performance_of(mat.getWidth(), mat.getHeight(), mat.getWidth(), run_time, iters));
+}
+
 int main(int argc, char** argv) {
   switch (DEVICE) {
   case CL_DEVICE_TYPE_DEFAULT: printf("DEVICE=DEFAULT\n"); break;
@@ -46,6 +62,7 @@ int main(int argc, char** argv) {
   }
 
   //runMatrixVectorMul();
-  runMatrixMatrixMul();
+  //runMatrixMatrixMul();
+  benchmark(10);
   return 0;
 }
